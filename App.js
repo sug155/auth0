dba00a01d09e56267e6877ef7ac9c9ed9feab9fd
sug155 <sugovnamani@gmail.com>
2024-11-21 00:55:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AuthLogin from './comp/authLogin'
+import AuthProfile from './comp/authProfile';
+
+
+const App = () => {
+  return (
+    <>
+      <AuthLogin />
+      
+      <AuthProfile />
+    </>
+  );
+}
+
+export default App
